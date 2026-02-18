2807d83e2d8ae9be4188347b42da64d8636f20bb
JBidaux <jeremie.bidaux@gmail.com>
2026-02-18 18:29:54 +0100

    exploring cltk and soup

--- main.py
+++ main.py
@@ -26,44 +26,37 @@ def keep_metadata(void):
 
         all_docs.append(line_doc)
 
+    # url: str = "https://dts.perseids.org/documents"
+    # params: str = {
+    #     "id": "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2",
+    #     "start": "1.1",
+    #     "end": "1.9"
+    # }
+
 def main():
-    url: str = "https://dts.perseids.org/documents"
-    params: str = {
-        "id": "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2",
-        "start": "1.1",
-        "end": "1.5"
-    }
-    response: str = requests.get(url, params=params)
+    url: str = "http://ftsr-dev.unil.ch:8000/api/dts/v1/document"
+    params: str = {"resource": "athous-iviron-450"}
+    headers: str = {"accept": "application/xml"}
+    response: str = requests.get(url, params=params, headers=headers)
 
     text_to_process: str = response.text
 
-    print(text_to_process)
+    # print(text_to_process)
 
     # Use the 'xml' parser specifically for TEI
     soup : bs4.BeautifulSoup = BeautifulSoup(text_to_process, features="xml")
-
-    print(soup.prettify())
-
+    # print(soup.body.prettify())
     nlp = NLP("grc", backend="stanza", suppress_banner=True)
 
-    # 1. Target the fragment/wrapper
-    # We use find() because 'dts:fragment' is the container for the text chunk
-    fragment = soup.find("dts:fragment") or soup.find("fragment")
+    # greek_text = soup.body.get_text(separator=" ", strip=True)
+    # doc = nlp.analyze(greek_text)
+    test_punct = nlp.analyze("ὁ δὲ, φίλιππος εἶπεν·")
 
-    if fragment:
-        # 2. Get text with a space separator
-        # This ensures "Ἀχιλῆος" and "οὐλομένην" don't get merged into one word
-        greek_text = fragment.get_text(separator=" ", strip=True)
+    # for word in doc.words:
+    #     print(f"Token: {word.string:<15} | Lemma: {word.lemma:<15} | POS: {word.upos} | IDSEN : {word.index_sentence}")
+    for word in test_punct.words:
+        print(f"Token: {word.string:<15} | Lemma: {word.lemma:<15} | POS: {word.upos} | IDSEN : {word.index_sentence}")
 
-        # 3. Clean up extra whitespace/newlines for the NLP
-        clean_greek = " ".join(greek_text.split())
-
-        print(clean_greek + '\n')
-
-        doc = nlp.analyze(clean_greek)
-        for sentence in doc.sentences:
-            for word in sentence.words:
-                print(f"Token: {word.string:<15} | Lemma: {word.lemma:<15} | POS: {word.upos}")
 
 if __name__ == "__main__":
     main()
